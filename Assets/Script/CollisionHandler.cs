2b18d22171f37cd625b75385b6902e1a870be40a
Svetlyi <svetlyi62@gmail.com>
2014-12-12 20:58:07 +0300

    new food, movement, collision handling

---
+++ Assets/Script/CollisionHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionHandler : MonoBehaviour {
+	public GameObject prefabFood;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.name == "Food") {
+			Destroy(col.gameObject);
+
+			Vector3 newPos = new Vector3(Random.Range(-4.0F, 2.0F), 4, Random.Range(-8.0F, 0F));
+			var newFood = Instantiate(this.prefabFood, newPos, transform.rotation);
+			newFood.name = "Food";
+		}
+	}
+}
---
+++ Assets/Script/KeyHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyHandler : MonoBehaviour {
+	private const float SPEED = 0.09f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 objPos = transform.position;
+		if (Input.GetKey ("right")) {
+			objPos.x += SPEED;
+		}
+		if (Input.GetKey ("left")) {
+			objPos.x -= SPEED;
+		}
+		if (Input.GetKey ("up")) {
+			objPos.y += SPEED;
+		}
+		if (Input.GetKey ("down")) {
+			objPos.y -= SPEED;
+		}
+		this.gameObject.transform.position = objPos;
+	}
+}
